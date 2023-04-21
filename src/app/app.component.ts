cba856d8e17d703158f6c8fb21b981bfe85efa18
Raphael <matosramosraphael@gmail.com>
2023-04-21 18:50:45 -0500

    ggg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Spotify';
+  nombre: string = 'Raphael'
+  edad: number = 22
+  telefono: null = null
+  direccion: undefined = undefined
+  otros: any;
+  car: CarModel = {
+    marca: 'Ford',
+    modelo: 'Focus',
+    ano: 2022
+  }
+  listaCar: Array<CarModel> = [
+    {
+      marca: 'Nissan',
+      modelo: 'Salon',
+      ano: 2020
+    },
+    {
+      marca: 'Toyota',
+      modelo: 'Silver 2000',
+      ano: 2021
+    }
+  ]
+}
+
+interface CarModel{
+  marca: string;
+  modelo: string;
+  ano: number;
 }
