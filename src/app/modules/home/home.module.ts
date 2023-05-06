edfc6f575fbfa3c9e680272ce0a6bbdd499770b5
Raphael <matosramosraphael@gmail.com>
2023-05-06 05:25:33 -0500

    primer shared visual

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SharedModule } from '@shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
-  ],
+  HomePageComponent
+    ],
   imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
-})
+  CommonModule,
+  HomeRoutingModule,
+  SharedModule
+    ]
+  })
+  
 export class HomeModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { MediaPlayeComponent } from './components/media-playe/media-playe.component';
+import { HeaderUserComponent } from './components/header-user/header-user.component';
 
 
 
 @NgModule({
   declarations: [
-    SideBarComponent,
-    MediaPlayeComponent
-  ],
+  SideBarComponent,
+  MediaPlayeComponent,
+  HeaderUserComponent
+    ],
   imports: [
-    CommonModule
-  ]
-})
+  CommonModule
+    ],
+  exports:[
+  SideBarComponent
+  
+    ]
+  })
+  
 export class SharedModule { }
